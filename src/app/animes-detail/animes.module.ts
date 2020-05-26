fdbd36fbd9287ee3c64f2c4dd272cc173808ecfe
Florian P <florian69780@hotmail.fr>
2020-05-26 18:18:27 +0200

    [CI] update conf -> update surge cmd

--- src/app/animes-detail/animes.module.ts
+++ src/app/animes-detail/animes.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AnimesRoutingModule } from './animes-routing.module';
-import {AnimesDetailComponent} from './animes-detail.component';
 
 @NgModule({
   declarations: [],
